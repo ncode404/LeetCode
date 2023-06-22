e86e331e253bb9fefd805217bbb9d202d78c5dc9
NAVEEN CHOKKAPU <91678670+naveen-chokkapu@users.noreply.github.com>
2023-06-22 09:37:04 +0530

    Time: 32 ms (5.95%) | Memory: 32.9 MB (5.04%) - LeetSync

--- generate-parentheses/generate-parentheses.cpp
+++ generate-parentheses/generate-parentheses.cpp
@@ -9,17 +9,26 @@ public:
         }
         return t==0;
     }
-    void add(int n,string s,vector<string> &ans) {
+    bool check(string s,int n) {
+        int leftcount=0,rightcount=0;
+        for(auto i:s) {
+            if(i=='(') leftcount++;
+            else rightcount++;
+        }
+        return (leftcount>n/2||rightcount>n/2);
+    }
+    void add(int n,string s,vector<string> &ans,int size) {
         if(n==0) {
             if(valid(s)) ans.push_back(s);
             return;
         }
-        add(n-1,s+'(',ans);
-        add(n-1,s+')',ans);
+        if(check(s,size)) return;
+        add(n-1,s+'(',ans,size);
+        add(n-1,s+')',ans,size);
     }
     vector<string> generateParenthesis(int n) {
         vector<string> ans;
-        add(2*n,"",ans);
+        add(2*n,"",ans,2*n);
         return ans;
     }
 };
