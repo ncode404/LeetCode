1f53fcdfc921201c34027f9b66dff1450814a4dc
NAVEEN CHOKKAPU <91678670+naveen-chokkapu@users.noreply.github.com>
2023-06-23 07:34:51 +0530

    Time: 93 ms (63.33%) | Memory: 26.8 MB (52.12%) - LeetSync

--- longest-substring-of-all-vowels-in-order/longest-substring-of-all-vowels-in-order.cpp
+++ longest-substring-of-all-vowels-in-order/longest-substring-of-all-vowels-in-order.cpp
@@ -3,21 +3,11 @@ public:
     int longestBeautifulSubstring(string word) {
         int a=0,e=0,iy=0,o=0,u=0,i,ans=0;
         for(i=0;i<word.size();) {
-            while(i<word.size() && word[i]=='a') {
-                a++; i++;
-            }
-            while(i<word.size() && word[i]=='e') {
-                e++; i++;
-            }
-            while(i<word.size() && word[i]=='i') {
-                iy++; i++;
-            }
-            while(i<word.size() && word[i]=='o') {
-                o++; i++;
-            }
-            while(i<word.size() && word[i]=='u') {
-                u++; i++;
-            }
+            while(i<word.size() && word[i]=='a') a++,i++;
+            while(i<word.size() && word[i]=='e') e++,i++;
+            while(i<word.size() && word[i]=='i') iy++,i++;
+            while(i<word.size() && word[i]=='o') o++,i++;
+            while(i<word.size() && word[i]=='u') u++,i++;
             if(a && e && iy && o && u) 
                 ans=max(ans,a+e+iy+o+u);
             a=e=iy=o=u=0;
