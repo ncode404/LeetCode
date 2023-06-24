500af4404ac0712e29b4d376b9088970bce6ce0c
NAVEEN CHOKKAPU <91678670+naveen-chokkapu@users.noreply.github.com>
2023-06-24 21:45:24 +0530

    Time: 3 ms (68.31%) | Memory: 6 MB (59.27%) - LeetSync

---
+++ fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int dp[31]={0};
+    int fib(int n) {
+        if(n<2) return n;
+        if(dp[n]) return dp[n];
+        return dp[n] = fib(n-1)+fib(n-2);
+    }
+};
