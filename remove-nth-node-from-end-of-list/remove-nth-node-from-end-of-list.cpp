ff581b7a4ac927dbf4621b778bfc18b8db7cce9a
NAVEEN CHOKKAPU <91678670+naveen-chokkapu@users.noreply.github.com>
2023-06-27 21:54:56 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (12.59%) - LeetSync

--- remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -10,23 +10,19 @@
  */
 class Solution {
 public:
-    void recursion(ListNode *&head,int k,int nodes,int &total,int &b) {
-        if(!head) {
-            total=nodes; return;
-        }
-        recursion(head->next,k,nodes+1,total,b);
-        if(nodes==(total-k-1)) {
-            if(head->next) {
-                head->next=head->next->next;
-                b=1;
-            }
-        }
-    }
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ListNode *copy = head;
-        int total=0,b=0;
-        recursion(copy,n,1,total,b);
-        if(!b) head=head->next;
-        return head;
+        ListNode *dummy = new ListNode();
+        dummy->next = head;
+        ListNode *fast = dummy,*slow = dummy;
+
+        for(int i=0;i<n;i++) 
+            fast = fast->next;
+        while(fast->next) {
+            slow = slow->next;
+            fast = fast->next;
+        }
+        slow->next = slow->next->next;
+
+        return dummy->next;
     }
 };
