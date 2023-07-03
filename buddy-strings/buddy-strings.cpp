faa39b22b0460f4a6ccd686af321482a41833f0b
NAVEEN CHOKKAPU <91678670+naveen-chokkapu@users.noreply.github.com>
2023-07-03 05:37:28 +0530

    Time: 3 ms (78.34%) | Memory: 7.1 MB (19.51%) - LeetSync

--- buddy-strings/buddy-strings.cpp
+++ buddy-strings/buddy-strings.cpp
@@ -3,15 +3,14 @@ public:
     bool buddyStrings(string s, string goal) {
         if(s.size()!=goal.size()) return 0;
         int c=0,t=0;
-        unordered_map<char,int> m,sm,gm;
+        unordered_map<char,int> sm,gm;
         for(auto i:s) sm[i]++;
         for(auto i:goal) gm[i]++;
 
         if(sm!=gm) return 0;
         for(int i=0;i<s.size();i++) {
             if(s[i]!=goal[i]) c++;
-            m[s[i]]++;
-            if(m[s[i]]>1) t++;
+            if(sm[s[i]]>1) t++;
         }
         if(c==2) return 1;
         else if(c!=0) return 0;
