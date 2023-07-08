4e63fbcc6b06a3f30ec684b8ec37dc35e81f6db1
NAVEEN CHOKKAPU <91678670+naveen-chokkapu@users.noreply.github.com>
2023-07-08 08:06:58 +0530

    Time: 39 ms (32.09%) | Memory: 18 MB (30.05%) - LeetSync

---
+++ combinations/combinations.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void f(int n,int k,int i,vector<vector<int>> &ans,vector<int> &temp) {
+        if(i==n+1) {
+            if(temp.size()==k) ans.push_back(temp);
+            return;
+        }
+        temp.push_back(i);
+        f(n,k,i+1,ans,temp); //pick
+        temp.pop_back();
+        f(n,k,i+1,ans,temp); //notpick
+    }
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+        f(n,k,1,ans,temp);
+        return ans;
+    }
+};
