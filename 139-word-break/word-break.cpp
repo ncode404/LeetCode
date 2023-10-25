3086bd3770af5a17e07f5ea28428e5ad00abf949
NAVEEN CHOKKAPU <91678670+naveen-chokkapu@users.noreply.github.com>
2023-10-25 15:13:35 +0530

    Time: 9 ms (54.62%) | Memory: 12.2 MB (49.71%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
-    int dp[301]={0};
     bool wordBreak(string s, vector<string>& wordDict) {
         unordered_map<string,int> m;
         for(auto i:wordDict) {
-            reverse(begin(i),end(i));
+            reverse(i.begin(), i.end());
             m[i]++;
         }
 
-        int i,j,n=s.size();
-        for(i=0;i<n;i++) {
-            string t;
-            for(j=i;j>=0;j--) {
-                t+=s[j];
-                if(m[t]) {
-                    if(j==0||(j>0 && dp[j-1]))
-                    {dp[i]=1; break;}
+        vector<bool> dp(s.size(),0);
+        for(int i=0;i<s.size();i++) {
+            string temp;
+            for(int j=i;j>=0;j--) {
+                temp+=s[j];
+                if(m[temp]) {
+                    if(j==0) {
+                        dp[i]=1; break;
+                    }
+                    else {
+                        if(dp[j-1]==1) {
+                            dp[i]=1; break;
+                        }
+                    }
                 }
             }
         }
-        return dp[n-1];
+        return dp[s.size()-1];
     }
 };
