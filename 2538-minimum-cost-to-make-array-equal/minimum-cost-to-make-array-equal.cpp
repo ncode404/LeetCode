820a8a3e2ef7968ee98cde515582065c2f623142
NAVEEN CHOKKAPU <91678670+naveen-chokkapu@users.noreply.github.com>
2023-11-01 19:53:16 +0530

    Time: 313 ms (5.06%) | Memory: 43.9 MB (27.76%) - LeetSync

--- 2538-minimum-cost-to-make-array-equal/minimum-cost-to-make-array-equal.cpp
+++ 2538-minimum-cost-to-make-array-equal/minimum-cost-to-make-array-equal.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     long long minCost(vector<int>& nums, vector<int>& cost) {
-        long long i,n=nums.size();
-        vector<pair<int,int>> v(n);
-        for(i=0;i<n;i++) 
-            v[i].first=nums[i], v[i].second=cost[i];
-
-        sort(v.begin(), v.end());
-        long long pre[n], suf[n], j=0, t=0;
-
-        pre[0]=v[0].second;
-        for(i=1;i<n;i++) pre[i]=pre[i-1]+v[i].second;
-        suf[n-1]=v[n-1].second;
-        for(i=n-2;i>=0;i--) suf[i]=suf[i+1]+v[i].second;
-
-        for(auto i:v) {
-            t += ((long long)i.second*(long long)abs(1-i.first));
+        vector<pair<int,int>> v;
+        for(int i=0;i<nums.size();i++) {
+            v.push_back({nums[i], cost[i]});
         }
-        long long res=t;
-        for(i=2;i<=1e6;i++) {
+        sort(v.begin(), v.end());
+        long long n=nums.size(), t=0, pre[n], suf[n],i;
+        pre[0]=v[0].second; suf[n-1]=v[n-1].second;
+        for(i=1;i<n;i++) {
+            pre[i]=pre[i-1]+v[i].second;
+        }
+        for(i=n-2;i>=0;i--) {
+            suf[i]=suf[i+1]+v[i].second;
+        }
+        for(auto i:v) {
+            t+=((long long)i.second*(long long)(i.first-1));
+        }
+        long long res=t, j=0;
 
+        for(int i=2;i<1e6;i++) {
             while(j<n && v[j].first<i) j++;
-            if(j>0) t += pre[j-1];
-            if(j<n) t -= suf[j];
+            if(j>0) t+=pre[j-1];
+            if(j<n) t-=suf[j];
 
             res=min(res,t);
         }
