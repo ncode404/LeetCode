fb3f4e272b955a642a8e4d6ec41a63478c8c04b0
NAVEEN CHOKKAPU <91678670+naveen-chokkapu@users.noreply.github.com>
2024-06-29 14:45:23 +0530

    Time: 896 ms (5.01%) | Memory: 22.6 MB (5.62%) - LeetSync

--- 633-sum-of-square-numbers/sum-of-square-numbers.py
+++ 633-sum-of-square-numbers/sum-of-square-numbers.py
@@ -1,15 +1,14 @@
 class Solution:
     def __init__(self):
-        self.__class__.d = {}
+        self.d = {}
         t = 0
         while t*t<=(2**31-1):
-            self.__class__.d[t*t]=1; t+=1
+            self.d[t*t]=1; t+=1
 
     def judgeSquareSum(self, c: int) -> bool:
-        # print(self.__class__.d)
         t = 0
         while t*t<=(2**31-1):
-            if c-(t*t) in self.__class__.d:
+            if c-(t*t) in self.d:
                 return 1
             t+=1
         return 0
