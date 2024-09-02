c86da69caa584d38955c688b8d487505f00cc0b8
NAVEEN CHOKKAPU <91678670+naveen-chokkapu@users.noreply.github.com>
2024-09-02 11:17:25 +0530

    Time: 673 ms (100.00%) | Memory: 288.4 MB (100.00%) - LeetSync

--- 3495-k-th-nearest-obstacle-queries/k-th-nearest-obstacle-queries.cpp
+++ 3495-k-th-nearest-obstacle-queries/k-th-nearest-obstacle-queries.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     vector<int> resultsArray(vector<vector<int>>& queries, int k) {
         vector<int> res;
-        multiset<int> s;
+        priority_queue<int> pq;
         for(auto i:queries) {
-            s.insert(abs(i[0])+abs(i[1]));
-            if(s.size()>k) s.erase(s.find(*s.rbegin()));
-            if(s.size()==k) res.push_back(*s.rbegin());
+            pq.push(abs(i[0])+abs(i[1]));
+            if(pq.size()>k) pq.pop();
+            if(pq.size()==k) res.push_back(pq.top());
             else res.push_back(-1);
         }
         return res;
