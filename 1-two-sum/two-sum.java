db603d0e09a7ad4ea8f80c654fcdf511ab1e7e5e
Naveen Chokkapu <91678670+ncode404@users.noreply.github.com>
2026-06-07 20:01:18 +0530

    Time: 4 ms (48.87%) | Memory: 46.6 MB (92.30%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,14 +1,17 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int temp[] = new int[2];
+        HashMap<Integer, Integer> hmap = new HashMap<Integer, Integer>();
         for(int i=0;i<nums.length;i++) {
-            for(int j=i+1;j<nums.length;j++) {
-                if(nums[i]+nums[j]==target) {
-                    temp[0]=i; temp[1]=j;
-                    return temp;
-                }
+            hmap.put(nums[i], i);
+        }
+        for(int i=0;i<nums.length;i++) {
+            if(hmap.containsKey(target-nums[i])) {
+                temp[0]=i; temp[1]=hmap.get(target-nums[i]);
+                if(temp[0]!=temp[1]) return temp;
             }
         }
         return temp;
+
     }
 }
