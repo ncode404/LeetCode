2ce166ba4734400463a0105fad4a20197d05e9d0
Naveen Chokkapu <91678670+ncode404@users.noreply.github.com>
2026-07-11 19:54:59 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (72.15%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -1,13 +1,10 @@
 class Solution {
     int check(int mid,vector<int> &nums) {
         if(mid==nums.size()-1) {
-            if(nums[mid]>nums[mid-1]) return 1;
-            return 0;
-        } else if(mid==0) {
-            if(nums[mid]>nums[mid+1]) return 1;
+            if(nums[mid]>nums[mid-1]) return 1; /// to check neighbours
             return 0;
         }
-        else if(nums[mid]>nums[mid+1]) return 1;
+        if(nums[mid]>nums[mid+1]) return 1;
         return 0;
     }
 public:
@@ -17,12 +14,9 @@ public:
         int low=0,high=nums.size()-1,ans=0;
         while(low<=high) {
             int mid=low+(high-low)/2;
-            cout<<low<<" "<<mid<<" "<<high<<" "<<ans<<"\n";
             if(check(mid,nums)) {
                 ans=mid; high=mid-1;
             } else low=mid+1;
-
-            cout<<low<<" "<<mid<<" "<<high<<" "<<ans<<"\n";
         }
         return ans;
     }
